8b6f922d0e9a065bb333f6ed25e9f9de93e316e6
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-15 15:19:47 +0100

    feat: index chatrooms

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Message.destroy_all
+Chatroom.destroy_all
+User.destroy_all
+
+john = User.create(email: 'mail@mail.com', password: '123123', name: 'John Doe', nickname: 'JD')
+michael = User.create(email: 'test@test.com', password: '123123', name: 'Michael Jackson', nickname: 'MJ')
+
+general = Chatroom.create(name: 'General')
+
+10.times do
+  author = [john, michael].sample
+  Message.create(chatroom: general, user: author, content: Faker::Movies::StarWars.quote)
+end
