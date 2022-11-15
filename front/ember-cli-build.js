655f61a8e610ef90a783d517d263ca3509695d1d
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-15 21:27:05 +0100

    tailwind installed

--- front/ember-cli-build.js
+++ front/ember-cli-build.js
@@ -1,10 +1,28 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {},
+          },
+          {
+            module: tailwind,
+            options: {
+              config: './tailwind.config.js',
+            },
+          },
+        ],
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
